91862bd84e1c2f8241bcfcd27f4f2e449b8711b7
frdnrdb <froden@gmail.com>
2017-02-27 01:08:01 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # elVis
 element visibility / inview / impression
+
+Usage: 
+  
+elVis(query, callback, {
+  time: 1000,         // INT: minimum milliseconds on screen, defaults to 500
+  part: 100           // INT: minimum percentage of element visible, defaults to 50
+  selector: 'attr'    // STRING: class|id|attr|tag, default to class
+}};
+  
---
+++ elVis.js
@@ -0,0 +1,100 @@
+  function elVis ( query, callback, config ) {
+  
+      config = config || {};
+    
+      if (typeof window === 'undefined' || typeof query !== 'string') return;    
+    
+      var visiblePercentage = config.part || 50,
+          visibleMilliseconds = config.time || 500,
+          selector = config.selector || 'class',
+          documentFunc = {
+            class: 'getElementsByClassName',
+            id: 'getElementById',
+            tag: 'getElementsByTagName',
+            attr: 'querySelectorAll'
+          },
+          elements = document[documentFunc[selector]](selector === 'attr' ? '['+query+']' : query);
+    
+      if (!elements || elements.length === 0) return;
+
+      var list = elements && elements.length ? [].slice.call(elements) : [elements],
+          size = list.length,
+          timerInterval = 100;
+  
+      list.forEach( checkElVis );
+
+  
+
+  
+      function checkElVis(el,i) {
+        
+          function check() {
+            
+            if (size === 0) {
+              window.removeEventListener('scroll', check);
+              callback('eventlistener off');            
+            }
+            
+            if ( isElVis(el) ) {
+              if ( !el.check ) {
+                if ( !el.vis ) el.vis = 0;
+                if ( !el.timer ) el.timer = setInterval( function(){ timer(el, i); }, timerInterval);
+              }
+            }
+            else if ( el.timer ) {
+              clearInterval( el.timer );
+              el.timer = undefined;
+            }
+            
+          }
+        
+          check();
+          window.addEventListener('scroll', check);
+
+      }
+  
+  
+  
+      function timer(el, i) {
+        
+        el.vis += timerInterval;
+        
+        if (el.vis > visibleMilliseconds) {
+          
+            el.check = true;
+            size--;          
+            clearInterval(el.timer);
+
+          
+          
+            var callbackObject = {
+              item: i+1+'/'+list.length, 
+              seen: new Date().toLocaleString(), 
+              props: {}
+            };
+            
+            [].slice.call(el.attributes).forEach(function(o){ if (o.nodeName !== 'style') callbackObject.props[o.nodeName] = o.nodeValue; });
+            
+            callback(callbackObject);
+          
+        }
+        
+      }     
+  
+  
+  
+      function isElVis(el) {
+        
+        var box = el.getBoundingClientRect(),
+            dom = window.innerHeight || doc.documentElement.clientHeight;
+        
+        if (box.bottom-(box.height*(visiblePercentage/100)) < 0 || box.top+(box.height*(visiblePercentage/100)) > dom) return false;
+        
+        return true;
+        
+      }    
+    
+  
+
+  }
+
